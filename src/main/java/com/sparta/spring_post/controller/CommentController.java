2242cf9e67658fecb9b28d9241eb10b60f851151
LEOJI <kjh11-07@hanmail.net>
2023-05-01 10:24:55 +0900

    Comment : Controller 주석 추가중

--- src/main/java/com/sparta/spring_post/controller/CommentController.java
+++ src/main/java/com/sparta/spring_post/controller/CommentController.java
@@ -9,9 +9,9 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 
-@RestController
-@RequestMapping("/api")
-@RequiredArgsConstructor
+@RestController                     // @RestController : 단순히 객체만들 반환하고 객체 데이터는 JSON 또는 XML 형식으로 HTTP 응답에 담아서 전송함
+@RequestMapping("/api")          // @RequestMapping : 공통되는 URL 이 있을 경우 메서드에 중복되는 value 값을 없앨 수 있음
+@RequiredArgsConstructor            // @RequiredArgsConstructor : final 이 붙거나 @NotNull 이 붙은 필드의 생성자를 자동 생성해주는 lombok 어노테이션
 public class CommentController {
 
     private final CommentService commentService;
@@ -25,6 +25,8 @@ public class CommentController {
     // 댓글 수정
     @PutMapping("/comment/{id}")
     public UserResponseDto<Comment> updateComment(@PathVariable Long id, @RequestBody CommentRequestDto commentRequestDto, HttpServletRequest httpServletRequest) {
+        // @PathVariable : URL 경로에 변수를 넣어주는 어노테이션
+        // @RequestBody : 클라이언트가 전송하는 JSON 형태의 HTTP Body 내용을 Java 객체로 변환시켜줌
         return commentService.updateComment(id, commentRequestDto, httpServletRequest);
     }
 
@@ -33,8 +35,5 @@ public class CommentController {
     public UserResponseDto<Comment> deleteComment(@PathVariable Long id, HttpServletRequest httpServletRequest){
         return commentService.deleteComment(id, httpServletRequest);
     }
-
-
-
 
 }
